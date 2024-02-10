9dd99a3b39aed01e755928513fc5e08c8782645d
Mehran Abdi <mehranabdi13793@gmail.com>
2024-02-10 18:43:17 +0330

    Add Input Service

---
+++ UI/src/Input/InputService.java
@@ -0,0 +1,14 @@
+package Input;
+
+import java.util.Scanner;
+
+public class InputService {
+        protected static Scanner scanner = new Scanner(System.in);
+    public static String readLine(){
+        var input = scanner.nextLine();
+        return input;
+    }
+    public static void closeScanner(){
+        scanner.close();
+    }
+}
